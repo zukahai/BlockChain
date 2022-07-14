f5d844cf98dd79a93ad26410adfaaee2d116efb4
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-07-14 16:20:46 +0700

    Update BlockChain.py

--- BlockChain.py
+++ BlockChain.py
@@ -24,6 +24,13 @@ def next_block(last_block):
     this_data = "I'm block " + str(this_index)
     this_hash = last_block.hash
     return Block(this_index, this_timestamp, this_data, this_hash)
+  
+def check_hacker(blockchain):
+  for i in range(1, len(blockchain)):
+    hash = Block.hash_block(Block(i, blockchain[i].timestamp, blockchain[i].data, blockchain[i - 1].hash))
+    if blockchain[i].hash != hash:
+      return "Block " + str(i) + " is hacked!"
+  return "Data safe!"
 
 blockchain = [create_genesis_block()]
 previous_block = blockchain[0]
@@ -35,8 +42,13 @@ for i in range(0, num_of_blocks_to_add):
   blockchain.append(block_to_add)
   previous_block = block_to_add
 
-#   print ("Block #{} added in blockchain".format(format(block_to_add.index, '3d')))
+  print ("Block #{} added in blockchain".format(format(block_to_add.index, '3d')))
+
 print ("Hash: {}\n".format(block_to_add.hash))
 
-for i in range(1, num_of_blocks_to_add):
-    print("{}\t{}".format(blockchain[i].hash, Block.hash_block(Block(i, blockchain[i].timestamp, blockchain[i].data, blockchain[i - 1].hash))))
+# for i in range(1, num_of_blocks_to_add):
+#     print("{}\t{}".format(blockchain[i].hash, Block.hash_block(Block(i, blockchain[i].timestamp, blockchain[i].data, blockchain[i - 1].hash))))
+
+print(check_hacker(blockchain))
+blockchain[3].data = "222"
+print(check_hacker(blockchain))
