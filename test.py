a4a6612ee43822fa8b17d37bdd84a9f074015726
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-07-15 13:41:24 +0700

    Update

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import json
 
-data = {"Eleven": "Millie","Mike": "Finn", "Will": "Noah"}
+f = open ('blocks.json', "r")
+  
+# Reading from file
+data = json.loads(f.read())
 
-print(data)
-
-with open('blocks.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+print(data[0]['data'])
