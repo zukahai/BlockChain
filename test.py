aed0aa0dd2f15f6ddbc2e7d413a504d436ff449b
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-07-15 12:12:32 +0700

    .

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ data = {"Eleven": "Millie","Mike": "Finn", "Will": "Noah"}
 
 print(data)
 
-with open('app.json', 'w', encoding='utf-8') as f:
+with open('blocks.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
